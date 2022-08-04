2c987089603b0bf8488d07e4b67c69651a8c2442
Ekkrest <abc96007@gmail.com>
2022-08-04 17:06:24 +0800

    SpawnManager

--- Assets/Scripts/Scripts2/LeaveRoom.cs
+++ Assets/Scripts/Scripts2/LeaveRoom.cs
@@ -1,24 +1,51 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LeaveRoom : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject LeaveRoomButton;
+    private GameObject LoserLabel;
 
-    void Awake()
+    [SerializeField]
+    private GameObject WinnerLabel;
+
+    [SerializeField]
+    private TMP_Text my_text;
+
+    void Start()
     {
-        LeaveRoomButton.SetActive(true);
+        Cursor.visible = true;
+
+        if (PlayerPrefs.GetInt("Status") == 0)
+        {
+            my_text.text = "GOT " + PlayerPrefs.GetString("killer") + " KILLED !!!";
+            
+            //WinnerLabel.SetActive(false);
+        }
+        else if (PlayerPrefs.GetInt("Status") == 1)
+        {
+           my_text.text = "YOU ARE CHAMPION !!!";
+        }
         Cursor.visible = true;
         Screen.lockCursor = false;
+        PlayerPrefs.DeleteAll();
     }
+
+
 
     public void GoBackLobby()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Open() 
+    {
+        gameObject.SetActive(true);
     }
 }
