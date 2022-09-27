6283b27acf5dc6502655a404df1516269b054003
Ekkrest <abc96007@gmail.com>
2022-09-27 23:47:38 +0800

    Ammo add

--- Assets/Scripts/Scripts2/SpawnManager.cs
+++ Assets/Scripts/Scripts2/SpawnManager.cs
@@ -29,19 +29,38 @@ public class SpawnManager : MonoBehaviour
         weapons[1] = new Weapon("RifleItem", 6);
         weapons[2] = new Weapon("ShotgunItem", 6);
         weapons[3] = new Weapon("SniperItem", 1);
+
+        Ammo[] ammos = new Ammo[4];
+        ammos[0] = new Ammo("PistolBullet", 20);
+        ammos[1] = new Ammo("RifleBullet", 20);
+        ammos[2] = new Ammo("ShotgunBullet", 20);
+        ammos[3] = new Ammo("SniperBullet", 20);
         bool flag = true;
+        int ammoCount = 12;
 
         int count = 0;
         if (PhotonNetwork.IsMasterClient) { 
             Debug.Log("一共有 " + spawnpoints.Length + " 個重生點");
             while (flag) 
             {
-                int num = Random.Range(0, 1000)%4;
-                if (weapons[num].getAmount() > 0) 
+                int num1 = Random.Range(0, 1000)%4;
+                int num2 = Random.Range(0, 1000)%4;
+                if (weapons[num1].getAmount() > 0) 
                 {
-                    CreateGun(count, weapons[num].getWeaponName());
-                    weapons[num].decreaceAmount();
+                    CreateGun(count, weapons[num1].getWeaponName());
+                    weapons[num1].decreaceAmount();
                     count++;
+                }
+
+                int randomPos = Random.Range(0, 1000) % 20;
+                if (ammoCount != 0) 
+                {
+                    if (ammos[num2].getAmount() > 0)
+                    {
+                        CreateAmmo(randomPos, ammos[num1].getAmmoName());
+                        weapons[num1].decreaceAmount();
+                        ammoCount++;
+                    }
                 }
 
                 if (count == 20) 
@@ -60,6 +79,16 @@ public class SpawnManager : MonoBehaviour
         spawnpoints[index].transform.position = pos;
         Transform spawnpoint = spawnpoints[index].transform;
         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs/Gun", str), spawnpoint.position, spawnpoint.rotation, 0, new object[] { pv.ViewID });
+    }
+
+    void CreateAmmo(int index, string str) 
+    {
+        Vector3 pos = spawnpoints[index].transform.position;
+        pos.y += 502;
+        pos.z += 40;
+        spawnpoints[index].transform.position = pos;
+        Transform spawnpoint = spawnpoints[index].transform;
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs/Ammo", str), spawnpoint.position, spawnpoint.rotation, 0, new object[] { pv.ViewID });
     }
 
     public Spawnpoint[] GetSpawnpoint()
